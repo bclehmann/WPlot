bdf46badec60795fb31941807b3b52f6112d84a7
Benjamin Lehmann <bclehmann@yahoo.ca>
2020-03-11 13:20:08 -0600

    tried

--- MenuBar.xaml.cs
+++ MenuBar.xaml.cs
@@ -9,6 +9,114 @@ namespace Where1.WPlot
 {
 	partial class MenuBar
 	{
+		private void PrefabSeries_Click(object sender, RoutedEventArgs e) {
+			string plotType = ((MenuItem)e.OriginalSource).Header.ToString().ToUpperInvariant();
+
+			PlotType type = new PlotType();
+			switch (plotType)
+			{
+				case "SIGNAL":
+					type = PlotType.signal;
+					break;
+			}
+
+			SettingsDialog settingsDialog = new SettingsDialog(type);
+			settingsDialog.Owner = App.Current.MainWindow;
+
+			DrawSettings drawSettings = new DrawSettings();
+
+			if (settingsDialog.ShowDialog() != true)
+			{//it is nullable
+				return;
+			}
+
+			drawSettings.colour = settingsDialog.plotColour;
+			drawSettings.drawLine = settingsDialog.shouldDrawLine.IsChecked == true; //Because it is nullable
+			drawSettings.drawLinearRegression = settingsDialog.linreg.IsChecked == true; //Because it is nullable
+			drawSettings.type = type;
+			drawSettings.label = settingsDialog.plotNameTextBox.Text;
+
+			string markerTypeName = settingsDialog.markerTypeComboBox.Text.ToUpperInvariant();
+			switch (markerTypeName)
+			{
+				case "FILLED CIRCLE":
+					drawSettings.markerShape = ScottPlot.MarkerShape.filledCircle;
+					break;
+				case "FILLED SQUARE":
+					drawSettings.markerShape = ScottPlot.MarkerShape.filledSquare;
+					break;
+				case "OPEN CIRCLE":
+					drawSettings.markerShape = ScottPlot.MarkerShape.openCircle;
+					break;
+				case "OPEN SQUARE":
+					drawSettings.markerShape = ScottPlot.MarkerShape.openSquare;
+					break;
+				case "FILLED DIAMOND":
+					drawSettings.markerShape = ScottPlot.MarkerShape.filledDiamond;
+					break;
+				case "OPEN DIAMOND":
+					drawSettings.markerShape = ScottPlot.MarkerShape.openDiamond;
+					break;
+				case "ASTERISK":
+					drawSettings.markerShape = ScottPlot.MarkerShape.asterisk;
+					break;
+				case "HASHTAG":
+					drawSettings.markerShape = ScottPlot.MarkerShape.hashTag;
+					break;
+				case "CROSS":
+					drawSettings.markerShape = ScottPlot.MarkerShape.cross;
+					break;
+				case "EKS":
+					drawSettings.markerShape = ScottPlot.MarkerShape.eks;
+					break;
+				case "VERTICAL BAR":
+					drawSettings.markerShape = ScottPlot.MarkerShape.verticalBar;
+					break;
+				case "TRI UP":
+					drawSettings.markerShape = ScottPlot.MarkerShape.triUp;
+					break;
+				case "TRI DOWN":
+					drawSettings.markerShape = ScottPlot.MarkerShape.triDown;
+					break;
+				case "NONE":
+					drawSettings.markerShape = ScottPlot.MarkerShape.none;
+					break;
+			}
+
+
+			Dictionary<string, object> metadata = new Dictionary<string, object>();
+
+			if (type == PlotType.signal)
+			{
+				SignalFrequencyDialog dlg = new SignalFrequencyDialog();
+				dlg.Owner = App.Current.MainWindow;
+				if (dlg.ShowDialog() != true)
+				{ //Nullable
+					return;
+				}
+
+				double sampleRate = 100;
+				double.TryParse(dlg.frequency, out sampleRate);
+
+				double xOffset = 0;
+				double.TryParse(dlg.xOffsetTextBox.Text, out xOffset);
+
+				metadata.Add("sampleRate", sampleRate);
+				metadata.Add("xOffset", xOffset);
+			}
+
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			if (openFileDialog.ShowDialog() == true)
+			{
+				PlotParameters plotParams = (App.Current as App).AddSeriesFromCSVFile(openFileDialog.FileName, drawSettings, metadata);
+				if (settingsDialog.errorDataCSV != null)
+				{
+					((App)App.Current).AddErrorFromCSVFile(plotParams, settingsDialog.errorDataCSV);
+				}
+				statusMessage.Text = $"{openFileDialog.FileName} loaded";
+			}
+		}
+
 		private void LoadCSVSeries_Click(object sender, RoutedEventArgs e)
 		{
 			string plotType = ((MenuItem) e.OriginalSource).Header.ToString().ToUpperInvariant();
